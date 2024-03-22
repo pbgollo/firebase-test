7f1db7b2083c2a4bb00f9a453c3d3ba0fddfe5f7
Pedro <pedrobgollo@gmail.com>
2024-03-22 13:37:37 -0300

    Conexão com o Banco

--- script.js
+++ script.js
@@ -19,3 +19,48 @@ btnPopup.addEventListener('click', ()=> {
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDjcUjtIDsuTI3N5ITWRTpWHygwcnfA2F8",
+    authDomain: "fir-test-8ebf9.firebaseapp.com",
+    databaseURL: "https://fir-test-8ebf9-default-rtdb.firebaseio.com",
+    projectId: "fir-test-8ebf9",
+    storageBucket: "fir-test-8ebf9.appspot.com",
+    messagingSenderId: "772220367580",
+    appId: "1:772220367580:web:885d8df18e5c354abcba19"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.database().ref("loginForm");
+
+document.getElementById("registerForm").addEventListener("submit", submitForm);
+ 
+function submitForm(e) {
+    e.preventDefault();
+
+    var name = getElementVal("name");
+    var email = getElementVal("email");
+    var password = getElementVal("password");
+
+    saveUser(name, email, password);
+
+    alert("Usuário cadastrado com sucesso.");
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+}
+
+const saveUser = (name, email, password) => {
+    var newUserForm = db.push();
+    newUserForm.set({
+        name: name,
+        email: email,
+        password: password,
+    })
+};
+
+const getElementVal = (id) => {
+    return document.getElementById(id).value
+};
