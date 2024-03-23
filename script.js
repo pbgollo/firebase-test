babf58c90a69ac967dd7b1e41052d5cb6f270717
Pedro Gollo <130512644+pbgollo@users.noreply.github.com>
2024-03-23 00:43:53 +0000

    Login

--- script.js
+++ script.js
@@ -18,7 +18,13 @@ btnPopup.addEventListener('click', ()=> {
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+    document.getElementById("loginForm").reset();
+    document.getElementById("registerForm").reset();
 });
+
+const getElementVal = (id) => {
+    return document.getElementById(id).value
+};
 
 const firebaseConfig = {
     apiKey: "AIzaSyDjcUjtIDsuTI3N5ITWRTpWHygwcnfA2F8",
@@ -32,7 +38,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-var db = firebase.database().ref("loginForm/" + email);
+var db = firebase.database();
 
 document.getElementById("registerForm").addEventListener("submit", submitForm);
  
@@ -47,22 +53,49 @@ function submitForm(e) {
 
     document.querySelector(".alert").style.display = "block";
 
-    document.getElementById("registerForm").reset();
+    setTimeout(() => {
+        document.getElementById("registerForm").reset();
+    }, 2000);
 
     setTimeout(() => {
         document.querySelector(".alert").style.display = "none";
     }, 5000);
-}
+};
 
 const saveUser = (name, email, password) => {
-    var newUserForm = db.push();
-    newUserForm.set({
+    db.ref("users/" + name).set({
         name: name,
         email: email,
         password: password,
     })
 };
 
-const getElementVal = (id) => {
-    return document.getElementById(id).value
-};
+document.getElementById("loginForm").addEventListener("submit", getUser);
+
+function getUser(e) {
+    e.preventDefault();
+
+    var name = getElementVal("loginName");
+    var password = getElementVal("loginPassword");
+
+    var user_ref = db.ref("users/" + name);
+
+    user_ref.once("value", function(snapshot){
+        var data = snapshot.val();
+        if (data) { // Verifica se o usuário existe
+            if (data.password === password) {
+                console.log("Login bem-sucedido!");
+                document.querySelector(".alertLogin").innerText = "Login bem-sucedido!";
+                document.querySelector(".alertLogin").style.color = "rgb(66,238,118)";
+            } else {
+                document.querySelector(".alertLogin").innerText = "Senha incorreta!";
+               document.querySelector(".alertLogin").style.color = "rgb(244,67,54)"; 
+            }
+        } else {
+            document.querySelector(".alertLogin").innerText = "Usuario não encontrado!";
+            document.querySelector(".alertLogin").style.color = "rgb((244,67,54)"; 
+        }
+    });
+
+    document.querySelector(".alertLogin").style.display = "block";
+}
